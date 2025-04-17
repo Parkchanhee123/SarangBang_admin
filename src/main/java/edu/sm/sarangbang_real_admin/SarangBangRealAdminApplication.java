874410bc564bcfe82cfdb097aa9e952bcb1e7d1a
chPark <pchh213@naver.com>
2025-04-17 16:37:06 +0900

    0417_박찬희_최초 commit

---
+++ src/main/java/edu/sm/sarangbang_real_admin/SarangBangRealAdminApplication.java
@@ -0,0 +1,13 @@
+package edu.sm.sarangbang_real_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarangBangRealAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarangBangRealAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sm/sarangbang_real_admin/SarangBangRealAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sm.sarangbang_real_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarangBangRealAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
